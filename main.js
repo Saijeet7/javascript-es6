2701dd2c5d6349d216702ac66c327099601fd5b3
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-05-16 12:07:48 +0545

    Complete : arrow function

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
-const myName = 'Saijeet';
+// const and let
+let myName = "Saijeet";
 console.log(myName);
 
-myName = 'Rockstar';
+myName = "Rockstar";
 console.log(myName);
+
+//Arrow Functions
+
+function printMyName(name) {
+  console.log(name);
+}
+
+printMyName("Max");
+
+const printMyNameArrow = (name) => {
+  console.log(name);
+};
+
+printMyNameArrow("Arrow");
+
+const printMyNameArrowEmpty = () => {
+  console.log("Max Empty");
+};
+
+printMyNameArrowEmpty();
+
+const printNameAge = (name, age) => {
+  console.log(name, age);
+};
+
+printNameAge("Max", 7);
+
+const multiply = (number) => number * number;
+console.log(multiply(9));
